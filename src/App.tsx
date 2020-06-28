8aff4e24bbba2bf93be4af99bbd1007ce9e5d82b
Troublemaker <1614922765@qq,com>
2020-06-28 18:01:29 +0800

    解决冲突

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Button, { ButtonSize, ButtonType } from './compontents/Button/button'
-<<<<<<< HEAD
 import Icon from './compontents/Icon/icon'
-import { library } from '@fortawesome/fontawesome-svg-core'
+import Menu from './compontents/Menu/Menu'
+import MenuItem from './compontents/Menu/MenuItem'
+import SubMenu from './compontents/Menu/SubMenu'
 // fas 指所有图标
+import { library } from '@fortawesome/fontawesome-svg-core'
 import { fas, faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 library.add(fas, faCheckSquare, faCoffee)
 
@@ -24,29 +26,6 @@ function App() {
       <Button disabled={true}>disabled</Button>
       <Button disabled>disabled</Button>
       <Icon icon='arrow-down' theme='danger' size='10x' />
-=======
-import Menu from './compontents/Menu/Menu'
-import MenuItem from './compontents/Menu/MenuItem'
-import SubMenu from './compontents/Menu/SubMenu'
-
-function App() {
-  return (
-    // <div className="App">
-    //   <p>size</p>
-    //   <Button size='lg'>large</Button>
-    //   <Button size='sm'>small</Button>
-    //   <Button >hello</Button>
-    //   <p>type</p>
-    //   <Button btnType='danger'>Danger</Button>
-    //   <Button btnType='default'>Default</Button>
-    //   <Button btnType='primary'>Primary</Button>
-    //   <Button btnType='text'>Primary</Button>
-    //   <Button btnType='link' href="http://baidu.com">Link</Button>
-    //   <p>disabled</p>
-    //   <Button disabled={true}>disabled</Button>
-    //   <Button disabled>disabled</Button>
-    // </div>
-    <div className='App'>
       <Menu onSelect={index => console.log(index)} mode='horizontal' defaultOpenSubMenus={['1']}>
         <MenuItem disabled>
           'MenuItem one'
@@ -72,9 +51,8 @@ function App() {
           'MenuItem five'
         </MenuItem>
       </Menu>
->>>>>>> Menu
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
